cca58f4207e80eab1b74dd87c6c8d493135a1950
Muhammad Hassan <mohammedh01060@gmail.com>
2025-08-13 03:15:58 +0300

    Fist version of implementation for EDA

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 **Welcome to HotelAtlas, where Machine Learning meets your next vacation spot.**
 
 In this project, i’m unleashing the full power of Machine Learning magic, both Supervised and Unsupervised, plus some SQL wizardry, all to decode the mysteries hidden in a massive hotel dataset.
+
 I guess now you know why i named it HotelAtlas as our journey needs to dive into the world of math and many different algorithms so we need an atlas.
 
-The dataset consists of approximately 70,000 records detailing various hotel attributes such as location, amenities, pricing, and ratings.
+The dataset consists of approximately 85,000 records detailing various hotel attributes.
 
 This project includes the following key components:
 
@@ -16,7 +17,7 @@ This project includes the following key components:
 
 - Utilizing SQL queries for data aggregation, ranking, and enrichment to support deeper analysis.
 
-Through HotelAtlas, the goal is to provide actionable insights into hotel characteristics and performance using rigorous machine learning and data engineering practices.
+Through HotelAtlas, the goal is to provide actionable insights into customer segments and customers stuff using into the hotels using machine learning and data engineering practices.
 
 ## Project Structure
 <pre>
---
+++ jupyter_notebooks/helpers.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def subplot_numerical_distributions(df:pd.DataFrame, columns:list):
+    """
+    Plot the distribution of multiple numerical columns in a DataFrame.
+    """
+    num_cols = len(columns)
+    fig, axes = plt.subplots(nrows=(num_cols + 1) // 2, ncols=2, figsize=(9, 3 * ((num_cols + 1) // 2)))
+    axes = axes.flatten()
+    
+    for i, column in enumerate(columns):
+        sns.histplot(df[column], kde=True, bins=30, ax=axes[i])
+        axes[i].set_title(f'Distribution of {column}')
+        axes[i].set_xlabel(column.title())
+        axes[i].set_ylabel('Frequency')
+    
+    for j in range(i + 1, len(axes)): 
+        fig.delaxes(axes[j])
+    
+    plt.tight_layout()
+    plt.show()
+
+def plot_categorical_feature(df:pd.DataFrame, column:str, top_n:int = 10):
+    """
+    This function to plot categorical columns data.
+    """
+    value_counts = df[column].value_counts()
+    value_counts = value_counts.head(top_n)
+
+    plt.figure(figsize=(8, 5))
+    sns.barplot(x=value_counts.values.astype(str), y=value_counts.index, palette="viridis")
+    plt.xlabel("Count")
+    plt.ylabel(column.title())
+    plt.title(f"Distribution of {column.title()}")
+    plt.tight_layout()
+    plt.show()
+
+def plot_categorical_counts(df:pd.DataFrame, column:str):
+    plt.figure(figsize=(8, 5))
+    sns.countplot(
+        x=df[column],  
+        data = df,
+        palette="crest"
+    )
+    plt.xlabel(column.title())
+    plt.ylabel("Count")
+    plt.title(f"Countplot for {column.title()}")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
